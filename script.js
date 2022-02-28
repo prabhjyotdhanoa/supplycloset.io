7186a967cbf739477f991fa8da0f72c4608cf927
Prabhjyot Dhanoa <prabhjyotdhanoa@gmail.com>
2022-02-28 18:32:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ angular.module('myModule').controller('DataAddController', function($scope) {
             iconClass: "mdi mdi-close-circle",
             buttonClass: "btn"
         },
-        pageLength: 20,
+        pageLength: 10,
     };
   
   vm.response = JSON.stringify({data:[]});
